762bfe49512cc5da8871a5d981efdbd929beb10f
mosman7 <mohamedosman998@hotmail.com>
2022-10-17 14:41:04 +0100

    bbc

---
+++ python_api.py
@@ -0,0 +1,9 @@
+#install requests
+#pip install requests # how to install package using python in terminal
+
+import requests
+# get requests
+request_bbc_status_code = requests.get("https://www.bbc.co.uk/iplayer/live/bbcnews")
+
+#checks outcome of
+print(request_bbc_status_code.status_code)
