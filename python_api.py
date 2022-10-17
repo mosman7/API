fb251f4dd3fa5e917e94738f776db05c82b88d60
mosman7 <mohamedosman998@hotmail.com>
2022-10-17 15:02:39 +0100

    bbc

--- python_api.py
+++ python_api.py
@@ -6,4 +6,17 @@ import requests
 request_bbc_status_code = requests.get("https://www.bbc.co.uk/iplayer/live/bbcnews")
 
 #checks outcome of API call
-print(request_bbc_status_code.status_code)
+#print(request_bbc_status_code.status_code)
+
+def request():
+    if request_bbc_status_code.status_code == 200:
+        print("Success")
+    else:
+        print("Unsuccessful")
+
+request()
+
+#prints the headers
+#print(request_bbc_status_code.headers)
+#checks the content available
+#print(request_bbc_status_code.content)
